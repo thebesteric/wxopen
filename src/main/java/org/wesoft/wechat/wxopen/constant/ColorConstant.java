5503c24394e14e6c117868bb3f77062387df4f21
eric <whatisjava@hotmail.com>
2020-02-24 11:35:55 +0800

    Added color constant class

---
+++ src/main/java/org/wesoft/wechat/wxopen/constant/ColorConstant.java
@@ -0,0 +1,62 @@
+package org.wesoft.wechat.wxopen.constant;
+
+public class ColorConstant {
+
+    /** 浅粉红 */
+    public static final String LIGHT_PINK = "#FFB6C1";
+
+    /** 粉红 */
+    public static final String PINK = "#FFC0CB";
+
+    /** 猩红 */
+    public static final String CRIMSON = "#DC143C";
+
+    /** 脸红的淡紫色 */
+    public static final String LAVENDER_BLUSH = "#FFF0F5";
+
+    /** 苍白的紫罗兰红色 */
+    public static final String PALE_VIOLET_RED = "#DB7093";
+
+    /** 热情的粉红 */
+    public static final String HOT_PINK = "#FF69B4";
+
+    /** 深粉色 */
+    public static final String DEEP_PINK = "#FF1493";
+
+    /** 适中的紫罗兰红色 */
+    public static final String MEDIUM_VIOLET_RED = "#C71585";
+
+    /** 兰花的紫色 */
+    public static final String ORCHID = "#DA70D6";
+
+    /** 蓟 */
+    public static final String THISTLE = "#D8BFD8";
+
+    /** 李子 */
+    public static final String PLUM = "#DDA0DD";
+
+    /** 紫罗兰 */
+    public static final String VIOLET = "#EE82EE";
+
+    /** 洋红 */
+    public static final String MAGENTA = "#FF00FF";
+
+    /** 纯蓝 */
+    public static final String BLUE = "#0000FF";
+
+    /** 适中的蓝色 */
+    public static final String MEDIUM_BLUE = "#0000CD";
+
+    /** 皇家蓝 */
+    public static final String ROYAL_BLUE = "#4169E1";
+
+    /** 紫色 */
+    public static final String PURPLE = "#800080";
+
+    /** 黑色 */
+    public static final String BLACK = "#000000";
+
+    /** 纯白 */
+    public static final String WHITE = "#FFFFFF";
+
+}

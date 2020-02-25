3e91a61ba24100f0d7ec49854b673822cc3666ae
eric <whatisjava@hotmail.com>
2020-02-25 23:21:09 +0800

    Add spring-boot-configuration-processor dependency

---
+++ src/main/java/org/wesoft/wechat/wxopen/constant/WechatProp.java
@@ -0,0 +1,24 @@
+package org.wesoft.wechat.wxopen.constant;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * TODO
+ *
+ * @author Eric Joe
+ * @version Ver 1.0
+ * @build 2020-02-25 20:18
+ */
+@Component
+@ConfigurationProperties(prefix = "wxopen.wechat")
+@Getter
+@Setter
+public class WechatProp {
+    private String appId;
+    private String appSecret;
+    private String token;
+    private String encodeAesKey = null;
+}
